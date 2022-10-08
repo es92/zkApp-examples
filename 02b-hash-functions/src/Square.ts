b8fa9042206884e4f4f1143c3b4c6d8d6f85f64c
Evan Shapiro <evanshapiro@Evans-Work-MacBook-Pro.local>
2022-10-07 23:35:26 -0500

    make initial 02b

---
+++ 02b-hash-functions/src/Square.ts
@@ -0,0 +1,35 @@
+import {
+  Field,
+  SmartContract,
+  state,
+  State,
+  method,
+  DeployArgs,
+  Poseidon,
+  Permissions,
+} from 'snarkyjs';
+
+export class Square extends SmartContract {
+  @state(Field) x = State<Field>();
+
+  deploy(args: DeployArgs) {
+    super.deploy(args);
+    this.setPermissions({
+      ...Permissions.default(),
+      editState: Permissions.proofOrSignature(),
+    });
+  }
+
+  @method init(firstSecret: Field) {
+    this.x.set(firstSecret);
+  }
+
+  @method incrementSecret(secret: Field) {
+    const x = this.x.get();
+    this.x.assertEquals(x);
+
+    Poseidon.hash([ secret ]).assertEquals(x);
+    this.x.set(Poseidon.hash([ secret.add(1) ]));
+  }
+
+}

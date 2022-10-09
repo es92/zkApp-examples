8c3ad925ce403bf1fa5fb3a270e01c52ceabee11
Evan Shapiro <evanshapiro@Evans-Work-MacBook-Pro.local>
2022-10-09 10:30:07 -0500

    update comments

--- 03-deploying-to-a-live-network/src/main.ts
+++ 03-deploying-to-a-live-network/src/main.ts
@@ -45,7 +45,7 @@ import {
 
   let zkapp = new Square(zkAppAddress);
   let x = await zkapp.num.fetch();
-  let isDeployed = (x != null && x.equals(0).not().toBoolean()); // is there a better way to check this? What if I had a zkApp that could take on any value in any of its state variables?
+  let isDeployed = (x != null && x.equals(0).not().toBoolean()); // is there a better way to check this? What if I had a zkApp that could take on any value in any of its state variables? -- null means its not a zkApp account -- maybe non null, and pull verification key and see if its the same? -- isDeployed to exist later
 
   console.log('isDeployed:', isDeployed);
 
@@ -54,16 +54,14 @@ import {
   if (!isDeployed) {
     console.log(`Deploying zkapp for public key ${zkAppAddress.toBase58()}.`);
     // the `transaction()` interface is the same as when testing with a local blockchain
-    // TODO re-deploys get "invalid fee excess" - why?
     let transaction = await Mina.transaction(
       { feePayerKey: deployerAccount, fee: transactionFee },
       () => {
-        AccountUpdate.fundNewAccount(deployerAccount);
-        // TODO shouldn't the init effect the verification key? Seems I can deploy discluding this (thereby putting the zkApp in state 0 - does this imply I could start the zkApp out in any state? Even a state that "should" need a proof to be reached?)
+        AccountUpdate.fundNewAccount(deployerAccount); // fund new account is putting funds towards a new account; if its not a new account then all the fees aren't accounted for
+        // TODO shouldn't the init effect the verification key? Seems I can deploy discluding this (thereby putting the zkApp in state 0 - does this imply I could start the zkApp out in any state? Even a state that "should" need a proof to be reached?) ---- there is an issue for this
         zkapp.init();
         zkapp.deploy({ zkappKey: zkAppPrivateKey, verificationKey });
-        zkapp.sign(zkAppPrivateKey);
-        // TODO why do I not need sign here in the default example?
+        // zkapp.sign(zkAppPrivateKey); -- not needed for a new deploy. Potentially needed for a redeploy TODO Nathan can explain more (Nathan mentioned something on nonces that could be explained)
       }
     );
     // if you want to inspect the transaction, you can print it out:
@@ -72,7 +70,7 @@ import {
     // send the transaction to the graphql endpoint
     console.log('Sending the deploy transaction...');
     const res = await transaction.send();
-    const hash = await res.hash(); // TODO how do I get errors when doing this? Or suppress printing graphql response?
+    const hash = await res.hash(); // TODO this has changed in the latest version. Has not been pushed to npm yet. Currently errors print to console. This may be optional, and possible to retrieve from cli in the latest version
     if (hash == null) {
       console.log('error sending transaction (see above)');
     } else {
@@ -103,7 +101,7 @@ import {
   // fill in the proof - this can take a while...
   console.log('Creating an execution proof...');
   const time0 = Date.now();
-  await transaction.prove(); // TODO this is taking ~1 minute on my machine... seems a bit slow for x -> x^2?
+  await transaction.prove(); // TODO this is taking ~1 minute on my machine... seems a bit slow for x -> x^2? Maybe there's a "base" cost?
   const time1 = Date.now();
   console.log('creating proof took', (time1 - time0)/1e3, 'seconds')
 
@@ -113,7 +111,7 @@ import {
   // send the transaction to the graphql endpoint
   console.log('Sending the transaction...');
   const res = await transaction.send();
-  const hash = await res.hash(); // TODO how do I get errors when doing this? Or suppress printing graphql response?
+  const hash = await res.hash(); // TODO this has changed in the latest version. Has not been pushed to npm yet.
   if (hash == null) {
     console.log('error sending transaction (see above)');
   } else {

02da9111535ce7961ff10d7373acab9aa9540a05
Evan Shapiro <evanshapiro@Evans-Work-MacBook-Pro.local>
2022-10-27 17:02:17 -0700

    Deploy new gh-pages

--- _next/static/BHEhfsewlqLKk4LQdFm6_/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ae0e3e027412e072.css","static/chunks/pages/index-9ac824c18ce4578d.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

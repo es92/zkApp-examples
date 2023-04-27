c4e24ae0dd3d2024974437b16acebc3901a6987c
gg <gg@mm.co>
2023-04-27 22:07:01 +1200

    Add "05-common-types"

--- 05-common-types/BasicMerkleTreeContract.ts
+++ 05-common-types/BasicMerkleTreeContract.ts
@@ -4,13 +4,7 @@ import {
   state,
   State,
   method,
-  DeployArgs,
-  Permissions,
   MerkleWitness,
-  Poseidon,
-  PublicKey,
-  Signature,
-  Circuit,
 } from 'snarkyjs';
 
 class MerkleWitness20 extends MerkleWitness(20) {}
@@ -18,22 +12,14 @@ class MerkleWitness20 extends MerkleWitness(20) {}
 export class BasicMerkleTreeContract extends SmartContract {
   @state(Field) treeRoot = State<Field>();
 
-  deploy(args: DeployArgs) {
-    super.deploy(args);
-    this.setPermissions({
-      ...Permissions.default(),
-      editState: Permissions.proofOrSignature(),
-    });
-  }
-
   @method initState(initialRoot: Field) {
     this.treeRoot.set(initialRoot);
   }
 
   @method update(
-    leafWitness: MerkleWitness20, 
+    leafWitness: MerkleWitness20,
     numberBefore: Field,
-    incrementAmount: Field,
+    incrementAmount: Field
   ) {
     const initialRoot = this.treeRoot.get();
     this.treeRoot.assertEquals(initialRoot);
@@ -45,11 +31,11 @@ export class BasicMerkleTreeContract extends SmartContract {
     rootBefore.assertEquals(initialRoot);
 
     // compute the root after incrementing
-    const rootAfter = leafWitness.calculateRoot(numberBefore.add(incrementAmount));
+    const rootAfter = leafWitness.calculateRoot(
+      numberBefore.add(incrementAmount)
+    );
 
     // set the new root
     this.treeRoot.set(rootAfter);
   }
 }
-
-
